38c45a3c803fba5d07b73e6292f8663a1d19c1f7
abrorjon016 <ikromjonovabrorjon3@gmail.com>
2024-07-18 11:44:09 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,131 @@
+// 1-masala
+// let num = +prompt("sonni kiriting");
+// if (num > 0) {
+//   num++;
+// } else {
+//   num;
+// }
+// console.log(num);
+
+// 2-masala
+// let num = +prompt("sonni kiriting");
+// if (num > 0) {
+//   num++;
+// } else {
+//   num -= 2;
+// }
+// console.log(num);
+
+// 3-masala
+// let num = +prompt("sonni kiritng");
+// if (num > 0) {
+//   num++;
+// } else if (num == 0) {
+//   num = 10;
+// } else {
+//   num -= 2;
+// }
+// console.log(num);
+
+
+// 4-masala
+// let a = +prompt('sonni kriting')
+// let b = +prompt('sonni kriting')
+// let c = +prompt('sonni kriting') 
+// let sum = 0
+// if (a>0) {
+//     sum++
+// }
+// if (b>0) {
+//     sum++
+// }
+// if (c>0) {
+//     sum++
+// }
+// console.log(sum);
+
+// 5-masala
+
+// let a = +prompt('sonni kriting')
+// let b = +prompt('sonni kriting')
+// let c = +prompt('sonni kriting')
+// let musbat =0
+// let manfiy =0
+// if (a>0) {
+//     musbat++
+// }else if (a<0) {
+//     manfiy++
+// }
+// if (b>0) {
+//     musbat++
+// }else if (b<0) {
+//     manfiy++
+// }
+// if (c>0) {
+//     musbat++
+// }else if (c<0) {
+//     manfiy++
+// }
+// console.log(musbat,'musbat');
+// console.log(manfiy,'manfiy');
+
+// 6-masala
+// let a = +prompt('sonni kriting')
+// let b = +prompt('sonni kriting')
+// let res=0;
+// if (a>b) {
+//     res=a
+// }else{
+//     res=b
+// }
+// console.log(res);
+
+// 7-masala
+// let a = +prompt('sonni kriting')
+// let b = +prompt('sonni kriting')
+// let res=0;
+// if (a<b) {
+//     res=a
+// }else{
+//     res=b
+// }
+// console.log(res);
+
+// 8-masala
+// let a = +prompt('sonni kriting')
+// let b = +prompt('sonni kriting')
+// let kata =0
+// let kichig =0
+// if (a>b) {
+//     kata=a
+// }else{
+//     kichig=a
+// }
+// if (a<b) {
+//     kata=b
+// }else{
+//     kichig=b
+// }
+// console.log(kata,'kata');
+// console.log(kichig,'kichig');
+
+// 9-masala
+// let a = +prompt('sonni kriting')
+// let b = +prompt('sonni kriting')
+// if (a>b) {
+//    b+=a
+// }
+// console.log(a,'a');
+// console.log(b,'b');
+
+// 10-masal
+// let a = +prompt('sonni kriting')
+// let b = +prompt('sonni kriting')
+// let res =0
+// if (a==b) {
+//     res+0
+// }
+// if (a!=b) {
+//     res=a+b
+// }
+    
